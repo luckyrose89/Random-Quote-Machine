dd733f1a4e790dc4888b4cca2f9a1f9be9d2ccfa
avidrutham <divyamathur89@gmail.com>
2018-10-18 17:29:16 +0530

    add tweet href to a tag

--- src/App.js
+++ src/App.js
@@ -1,50 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import QuoteBox from "./QuoteBox";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.handleQuoteChange = this.handleQuoteChange.bind(this);
-
-    this.state = {
-      quote: "",
-      author: ""
-    };
   }
-  componentDidMount() {
-    fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
-      .then(response => response.json())
-      .then(
-        result => {
-          this.setState({
-            quote: result[0]["quote"],
-            author: result[0]["character"]
-          });
-        },
-        err => {
-          this.setState({
-            quote: "error",
-            err,
-            author: "error"
-          });
-        }
-      );
-  }
-
   handleQuoteChange() {
     window.location.reload();
   }
-
   render() {
-    const { quote, author } = this.state;
     return (
       <div>
         <h1>Random Quote Machine</h1>
-        <p>{quote}</p>
-        <p>{author}</p>
-        <button onClick={this.handleQuoteChange}>New</button>
-        <button>Tweet</button>
+        <QuoteBox />
+        <div>
+          <button onClick={this.handleQuoteChange}>New</button>
+          <a href="twitter.com/intent/tweet">Tweet</a>
+        </div>
       </div>
     );
   }

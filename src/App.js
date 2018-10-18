904f609f7b3cc8a31684b3ba37397dcf1a755942
avidrutham <divyamathur89@gmail.com>
2018-10-18 14:53:53 +0530

    add quote box to app

--- src/App.js
+++ src/App.js
@@ -1,2 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import QuoteBox from "./QuoteBox";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Random Quote Machine</h1>
+        <QuoteBox />
+        <button>New</button>
+        <button>Tweet</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
